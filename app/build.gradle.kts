2cdb70f7200cd959c567901f688c1f891b63d3f6
TutorialsAndroid <akshaysunilmasram@yahoo.com>
2026-06-03 16:11:52 +0530

    Bump version to v3.0.11
    - Updated `versionCode` to 12 and `versionName` to `3.0.11` in `app/build.gradle.kts`.
    - Updated `PUBLISH_VERSION` to `3.0.11` in `toastlibrary/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.app.toast"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
-        versionName = "3.0.10"
+        versionCode = 12
+        versionName = "3.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- toastlibrary/build.gradle.kts
+++ toastlibrary/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 group= "com.github.TutorialsAndroid"
 
 extra["PUBLISH_GROUP_ID"] = "io.github.tutorialsandroid"
-extra["PUBLISH_VERSION"] = "3.0.10"
+extra["PUBLISH_VERSION"] = "3.0.11"
 extra["PUBLISH_ARTIFACT_ID"] = "toastlibrary"
 
 extra["PUBLISH_DESCRIPTION"] = "ToastLib is a lightweight Android library that provides customizable, modern toast messages with icons, colors, gravity control, and clean API usage. It works reliably on Android 11+ using custom layouts instead of deprecated system toast styling."
